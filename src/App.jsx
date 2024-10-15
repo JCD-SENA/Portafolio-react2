3e898703d15fb2659a7c1b2930522af4b25c0b47
SENA <SENA@ARMSCCCCTGOD163.FormacionSena.RED>
2024-10-15 09:08:56 -0500

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Header } from "./components/Layouts/Header/Header"
+import { Main } from "./components/Layouts/Main/Main"
+import { Footer } from "./components/Layouts/Footer/Footer"
+
+export const App = () => {
+	return (
+		<>
+			<Header />
+			<Main />
+			<Footer />
+		</>
+	)
+}
---
+++ src/components/Layouts/Header/Header.jsx
@@ -0,0 +1,10 @@
+import { Navbar } from "../../UI/Navbar/Navbar"
+
+export const Header = () => {
+	return (
+		<header>
+			<h1>Juan Camilo</h1>
+			<Navbar />
+		</header>
+	)
+}
